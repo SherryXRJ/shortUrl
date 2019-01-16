ca8de99750fcc96daca7bdc2d1689e6c370b668d
xurunjie <xurunjie@migu.cn>
2019-01-16 09:41:17 +0800

    modify sql

--- sql/short_url.sql
+++ sql/short_url.sql
@@ -1,47 +1,18 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 80011
- Source Host           : localhost:3306
- Source Schema         : short_url
-
- Target Server Type    : MySQL
- Target Server Version : 80011
- File Encoding         : 65001
-
- Date: 15/01/2019 23:39:52
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for config
--- ----------------------------
 DROP TABLE IF EXISTS `config`;
-CREATE TABLE `config`  (
+CREATE TABLE `config` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `key` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '配置 key',
-  `value` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '配置 value',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  `key` varchar(10) DEFAULT NULL COMMENT '键',
+  `value` varchar(100) DEFAULT NULL COMMENT '值',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of config
--- ----------------------------
 INSERT INTO `config` VALUES (1, 'short_len', '6');
 
--- ----------------------------
--- Table structure for url
--- ----------------------------
-DROP TABLE IF EXISTS `url`;
-CREATE TABLE `url`  (
-  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `short_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '短链接',
-  `long_url` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '长链接',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
-SET FOREIGN_KEY_CHECKS = 1;
+DROP TABLE IF EXISTS `url`;
+CREATE TABLE `url` (
+  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `short_url` varchar(255) DEFAULT NULL COMMENT '短链接',
+  `long_url` varchar(1000) DEFAULT NULL COMMENT '长链接',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
